84932834a7b0a5ec23bdea9da783bfc7b6b2dfc2
AlReznik <70607049+AlReznik@users.noreply.github.com>
2021-04-04 14:35:03 +0200

    commit on board

--- hello2.cpp
+++ hello2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <map>
+#include <set>
 using namespace std;
 
-int* parseString(string str)
+/* int* parseString(string str)
 {
     map<string,int> dict = {{"a",0},{"b",1},{"c",2},{"d",3},{"e",4},{"f",5},{"g",6},{"h",7}};
     int* ar = new int[4];
@@ -18,8 +19,47 @@ public:
     int age;
     Person(string name, int age){this->name=name;this->age=age;}
 };
+ */
 
-int main(){
+/* class Punkt {  
+public:
+    Punkt(float x, float y)
+    { 
+        this->x = x;
+        this->y = y;
+    }  
+    static void makePunkt(float x, float y)
+    { 
+        if(x*x+y*y<=1)
+        {
+            Punkt(x,y);
+            std::cout << "Punkt utworzony pomyslnie"<<std::endl;
+        }
+        else
+        {
+            std::cout << "Poza okregiem jednostkowym!"<<std::endl;
+        }
+    }
+
+private:   
+    float x;
+    float y;
+}; */
+
+std::set<std::string> dane = {"a1","f2","h3"};
+
+int main() {   
+  
+    for(std::string item : dane)
+    {
+        std::cout << item <<std::endl;
+    }
+    // std::cout << "Prawe oko misia ma color " << misio.prawe.dajKolor() <<std::endl;
+
+} 
+
+
+/* int main(){
     Person xx("Mike", 25);
     int x[] = {1,2,3};
     int y = 10;
@@ -30,14 +70,14 @@ int main(){
     int *z = new int[3];
     z[0] = 5;
 
-/*     for (int i = 0; i<4; i++)
+    for (int i = 0; i<4; i++)
     {
         cout << parseString("e2-g4")[0] << " " <<parseString("e2-g4")[1] << endl;
         cout << parseString("e2-g4")[3] << " " <<parseString("e2-g4")[4] << endl;
-    } */
+    }
     cout << *p << " " << r[2] <<" " << nik->age <<" " << (*s).name << endl;
     cout << *x << " " << x[0] <<" " << nik->age <<" " << (*s).name << endl;
     cout << x << " " << x[0]+1 <<" " << xx.name <<" " << x+1 << endl;
     cout << z << " " << z[0] <<" " << &z[0] <<" " << z[1] << endl;
     return 0;
-}
+} */
